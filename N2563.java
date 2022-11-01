0f449d5dc850b05f462722fe2362216604467623
HongGeonHo <booklike112@gmail.com>
2022-11-01 23:23:15 +0900

    Java Practice

---
+++ N2563.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N2563 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr =new int[100][100];
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for(int j = x; j < x + 10; j++) {
+                for(int k = y; k < y + 10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < 100; i++) {
+            for(int j = 0; j < 100; j++) {
+                if(arr[i][j] == 1) count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
