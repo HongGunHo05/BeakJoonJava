1b9fd473ca2380839e132f2a489b7d03bf933986
HongGunHo05 <booklike112@gmail.com>
2022-12-05 16:18:21 +0900

    Java Practice

---
+++ N17451.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N17451 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        long[] v = new long[n];
+        for (int i = 0; i < n; i++) {
+            v[i] = Long.parseLong(st.nextToken());
+        }
+
+        long s = v[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+
+            if(v[i] == 0){
+                continue;
+            }
+
+            if (s < v[i]) {
+                s = v[i];
+            } else if (v[i] < s) {
+
+                s = ((s / v[i]) + 1) * v[i];
+
+            }
+
+        }
+
+        System.out.println(s);
+
+    }
+}
